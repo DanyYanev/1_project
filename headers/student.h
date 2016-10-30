84a7cc09384c7e7179f8b15225a8e73a909c474f
DanyYanev <yanev89@gmail.com>
2016-10-30 14:11:18 -0700

    Almost done struct

--- headers/student.h
+++ headers/student.h
@@ -1,12 +1,67 @@
 #ifndef STUDENT_H
 #define STUDENT_H
 
-typedef struct{
-    int num;
-    char name[50];
-    int marks[20];
-    float avr;
-}student;
+class Employee{
+    protected:
+        string name;
+        double pay, Opay, salary, Oh, coef, workddays, workdays;
+    public:
+        Employee() {
+            name = "";
+            pay = 0; //per hours
+            Opay = 0;
+            salary = 0;
+            Oh = 0;
+            coef = 1.75;
+            workdays = 0;
+            workddays = 0;
+        }
+        Employee (string empName){
+            name = empName;
+        }
+        ~Employee(){}
+        string getName() {
+            return name;
+        }
+        void setName(string empName){
+            name = empName;
+        }
+        double getSalary(){
+            return salary;
+        }
+        void setWday(double days){
+          workdays = days; //work days for the month
+        }
+        void setWddays(double days){
+          workddays = days; //worked days
+        }
+        void setSalary(double MSalary){
+            salary = MSalary; //monthly salary
+        }
+        double setOverHours(double oh){
+          Oh = oh; //overhours
+        }
+        double setOcoef(double coef){
+          Ocoef = coef;
+        }
+        double getOPay(){
+          return Oh*pay*Ocoef;
+        }
+        double getPay(){
+          return pay*workddays/8;
+        }
+        double getWage(){
+            int wage = getPay() + getOPay();
+            return wage;
+        }
+        /*string toString(){
+            stringstream stm;
+            stm << name << ": ";
+            return stm.str();
+            }
+        */
+
+};
 
 
 #endif
